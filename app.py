4358fdb4d996e1971310e21ed0a3fb08fc2a0fb9
bb8311 <3q311137@gmail.com>
2025-11-21 02:11:06 +0000

    1

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask,request,render_template
+# flash
+
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/",methods = ["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return(render_template("index.html"))
+
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    q = request.form.get("q")
+    print(q)
+    return(render_template("main.html"))
 
 if __name__ == "__main__":
     app.run()
-
